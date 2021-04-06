aa6c3a091b40e8c4724f2ea8be79bc9d899d2882
wkirk01 <kirk.william.c@gmail.com>
2021-04-05 21:12:34 -0500

    Simplify matchValues

--- database.go
+++ database.go
@@ -319,11 +319,12 @@ func searchDocuments(query map[string]interface{}, documents []map[string]interf
 }
 
 func matchValues(queryValue interface{}, documentValue interface{}) bool {
-	if IsString(queryValue) || IsNumber(queryValue) || IsBoolean(queryValue) || IsNil(queryValue) {
-		return queryValue == documentValue
+
+	if queryValue == documentValue {
+		return true
 	}
 
-	if IsFunction(queryValue) {
+	if reflect.TypeOf(queryValue).Kind() == reflect.Func {
 		queryFunc := reflect.ValueOf(queryValue).Interface().(QueryFunc)
 		return queryFunc(documentValue)
 	}
